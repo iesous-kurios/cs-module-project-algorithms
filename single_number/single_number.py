930b54d5c61bee77b5e58518f6b998bcef55c9c0
iesous-kurios <j.wylie.81@gmail.com>
2020-10-06 20:46:07 -0700

    finished single_number

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -3,7 +3,16 @@ Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
 def single_number(arr):
-    # Your code here
+    
+    for i in range(0, len(arr)-1):
+        count = 0
+        for ii in arr:
+            
+            if ii == arr[i]:
+                count += 1
+
+        if count <= 1:
+            return arr[i]    
 
     pass
 
